9e2732dbdd4121a21c8f9bb798bfc7ccd4fc5dd0
Abel <laroussi.abel@hotmail.fr>
2025-03-23 00:39:42 +0100

    Made iframe took the whole page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 const { invoke } = window.__TAURI__.tauri;
 
+function changeSrc(src) {
+  const iframe = document.querySelector("iframe");
+  iframe.setAttribute("src", src); // Set the new iframe src
+}
+
 let greetInputEl;
 let greetMsgEl;
 
-window.addEventListener("DOMContentLoaded", () => {
-});
+window.addEventListener("DOMContentLoaded", () => {});
